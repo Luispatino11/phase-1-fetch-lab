c723527da7504577bf6430d1746aff6c209efc13
luis patino <lcp.patino@gmail.com>
2023-05-14 18:38:18 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      return response.json();
+    })
+    .then(data => renderBooks(data))
+    .catch(error => console.log(error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.getElementById('book-list');
+
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement('li');
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+window.addEventListener('load', fetchBooks);
+
+// Return the fetch() request from fetchBooks()
+export { fetchBooks };
